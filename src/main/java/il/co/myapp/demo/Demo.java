d9c812e2f0f2917a7e4999c2453a55c5ded1e579
artmeleshko <artem.meleshko@gmail.com>
2017-10-27 00:09:00 +0300

    My second commit

--- src/main/java/il/co/myapp/demo/Demo.java
+++ src/main/java/il/co/myapp/demo/Demo.java
@@ -3,7 +3,7 @@ package il.co.myapp.demo;
 public class Demo {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        System.out.println("Hello World!!!");
 
     }
 
